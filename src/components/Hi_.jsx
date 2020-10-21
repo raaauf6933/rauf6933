4d0fc4f762e9ae8538d27b7b9ee4421592ffc6bb
raaauf6933 <53444557+raaauf6933@users.noreply.github.com>
2020-10-21 17:47:10 +0800

    edit hi component

--- src/components/Hi_.jsx
+++ src/components/Hi_.jsx
@@ -6,33 +6,31 @@ class Hi extends Component {
   render() {
     return (
       <div>
-      
-          <section name="hi" className="container section1">
-            <div className="row">
-              <div className="col-md-12 ">
-                <h1
-                  className="section__title"
-                  style={{ textIndent: "0px", fontWeight: "bolder" }}
-                >
-                  Hi_
-                </h1>
-                <div className="container">
-                  <p className="section__description">
-                  I am an aspiring Software developer who is able to build applications 
-                  on both web and mobile application from concept, navigation, layout. 
-                  Skilled at writing well-designed testable and efficient code 
-                  using current best practices in application development. 
-                  </p>
+        <section name="hi" className="container section1">
+          <div className="row">
+            <div className="col-md-12 ">
+              <h1
+                className="section__title"
+                style={{ textIndent: "0px", fontWeight: "bolder" }}
+              >
+                Hi_
+              </h1>
+              <div className="container">
+                <p className="section__description">
+                  I am an aspiring Software Developer who is able to build
+                  applications on web from concept, navigation, layout. Skilled
+                  at writing well-designed testable and efficient code using
+                  current best practices in application development.
+                </p>
 
-                  <a className="section_btn site-btn" href="/#">
-                   Download CV
-                  </a>
-                </div>
+                <a className="section_btn site-btn" href="/#">
+                  Download CV
+                </a>
               </div>
             </div>
-            <hr></hr>
-          </section>
-        
+          </div>
+          <hr></hr>
+        </section>
       </div>
     );
   }
